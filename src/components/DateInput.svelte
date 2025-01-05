ce73933db0b706f2d9fd4a39fd269a9e9fbfc97e
Eduardo Alejandro González Martell <eduardoglez64377@gmail.com>
2025-01-04 23:29:19 -0500

    fix: input control form

--- src/components/DateInput.svelte
+++ src/components/DateInput.svelte
@@ -1,25 +1,26 @@
 <script lang="ts">
-  import { Calendar } from 'lucide-svelte';
-  
-  export let value: Date | null = null;
-  
-  function handleInput(event: Event) {
-    const input = event.target as HTMLInputElement;
-    value = input.value ? new Date(input.value) : null;
-  }
+    import {Calendar} from 'lucide-svelte';
+
+    export let value: Date | null = null;
+
+    function handleInput(event: Event) {
+        const input = event.target as HTMLInputElement;
+        value = input.value ? new Date(input.value) : null;
+    }
 </script>
 
 <div class="form-control w-full max-w-xs mx-auto">
-  <label class="label">
+    <label class="label" for="birthdate">
     <span class="label-text flex items-center gap-2">
-      <Calendar class="text-primary" />
+      <Calendar class="text-primary"/>
       Select your birth date
     </span>
-  </label>
-  <input
-    type="date"
-    class="input input-bordered w-full"
-    on:input={handleInput}
-    max={new Date().toISOString().split('T')[0]}
-  />
+    </label>
+    <input
+            id="birthdate"
+            type="date"
+            class="input input-bordered w-full"
+            on:input={handleInput}
+            max={new Date().toISOString().split('T')[0]}
+    />
 </div>
